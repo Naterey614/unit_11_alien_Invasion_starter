307b66f11feecfcf06dc5c478e7488d87369fbe2
Naterey614 <nathanericrey@live.com>
2025-04-14 13:44:06 -0400

    Refactored game loop for events and drawing

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,18 +21,25 @@ class AlienInvasion:
     
         self.ship = Ship(self)
 
-    def run_game(self):
+    def run_game(self) -> None:
         # Game loop
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                    pygame.quit()
-                    sys.exit()
-            self.screen.blit(self.bg, (0,0))
-            self.ship.draw()
-            pygame.display.flip()
+            self._check_events()
+
+            self._update_screen()
             self.clock.tick(self.settings.FPS)
+
+    def _update_screen(self) -> None:
+        self.screen.blit(self.bg, (0,0))
+        self.ship.draw()
+        pygame.display.flip()
+
+    def _check_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
 
 
 if __name__ == '__main__':
